8ab43384ae520bea46dd8eb1fee1fc27f469cad3
Aaaasush <sushmitpardeshi@gmail.com>
2025-05-09 10:12:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function myFun(selectid) {
+    if (selectid.style.display === "inline") {
+        selectid.style.display = "none";
+    } else {
+        selectid.style.display = "inline";
+    }
+}
